27ba943b3781c3cd4d1417adea0b3df29213ad4c
Jesuloba <jesulobajohn@gmail.com>
2023-10-21 07:41:36 +0100

    immutable

--- main.go
+++ main.go
@@ -2,65 +2,31 @@ package main
 
 import (
 	"log"
-	"net/http"
+	"log/slog"
 
 	"github.com/gofiber/fiber/v2"
+
 )
 
-type Item struct {
-	ID    int     `json:"id"`
-	Title string  `json:"title"`
-	Price float64 `json:"price"`
-}
-
-var items []Item = []Item{
-	{1, "Item 1", 10.9},
-	{2, "Item 2", 11.9},
-	{3, "Item 3", 20.4},
-}
-
 // returns all items in a list
-func getAllItems(c *fiber.Ctx) error {
-
-	return c.JSON(items)
-}
-
-// returns a single item
-func getItemById(c *fiber.Ctx) error {
-
-	id, err := c.ParamsInt("id")
-	if err != nil {
-		return c.Status(http.StatusBadRequest).SendString("Invalid ID")
-	}
-
-	for _, item := range items {
-		if item.ID == id {
-			return c.JSON(item)
-		}
-	}
-
-	return c.Status(http.StatusNotFound).SendString("Item not found")
-}
-
-func createItem(c *fiber.Ctx) error {
-	var item Item
-	if err := c.BodyParser(&item); err != nil {
-		return c.Status(http.StatusBadRequest).SendString("Invalid request")
-	}
-
-	item.ID = len(items) + 1
-	items = append(items, item)
-	return c.JSON(item)
+func getHandler(c *fiber.Ctx) error {
+	n := c.Params("name")
+	slog.Info("request received", "name", n)
+	return nil
 }
 
 func main() {
-	app := fiber.New()
+	appConfig := fiber.Config{
+		AppName:           "My Awesome App v0.0.0-beta",
+		EnablePrintRoutes: true,
+		ServerHeader:      "Awesome Server",
+		Immutable: true,
+	}
+	app := fiber.New(appConfig)
 
-	app.Get("/items", getAllItems)
-	app.Get("/items/:id", getItemById)
-	app.Post("/items", createItem)
+	app.Get("/:name", getHandler).Name("Test")
 
-	log.Print("starting app on port :3000")
+	slog.Info("starting app on port :3000")
 	err := app.Listen(":3000")
 	if err != nil {
 		log.Fatal(err)
